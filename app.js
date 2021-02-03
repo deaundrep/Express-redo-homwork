8b40ecf35430139192e3a3201ffcc497b31bdc65
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2021-02-03 12:17:48 -0500

    add delete

--- app.js
+++ app.js
@@ -4,18 +4,28 @@ const PORT = process.env.PORT || 3000;app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 let teamArray = [
-{
+    {
     id: 1,
     name: "lakers",
-    playersArray: [],
-},
+    playersArray: [
+        {
+        player: "kobe",
+        },
+        {
+        player: "shaq",
+        },
+    ],
+    },
 ];
 
 app.get("/", function (req, res) {
 res.status(200).send("Welcome to our first API");
-});app.get("/team", function (req, res) {
+});
+app.get("/team", function (req, res) {
+    console.log(req.query)
 res.status(200).json({
     teamArray,
+    query: req.query,
 });
 });
 
@@ -38,17 +48,108 @@ app.get("/team/:teamID", function (req, res) {
 });
 
 app.post("/team/add-players/:teamID", function (req, res) {
-    
-let teamIDNumber = Number(req.params.teamID);
-
-teamArray.forEach((team) => {
-    if (team.id === teamIDNumber){
-        team.playersArray.push(req.body);
+    let teamIDNumber = Number(req.params.teamID);
+    let message;
+    let targetTeam;
+    let teamIndex;
+    teamArray.forEach((team, index) => {
+    if (team.id === teamIDNumber) {
+        let singleTeamArray = team.playersArray;
+        singleTeamArray.push(req.body);
+        targetTeam = team;
+        teamIndex = index;
+        return;
     }
-});
-res.status(200).json({
+    });
+    //console.log(JSON.stringify(teamArray));
+    //set the playersARray to targetTeamArray
+    let targetTeamArray = targetTeam.playersArray;
+    //return only players array
+    let playersNameArray = targetTeamArray.map(function (item) {
+    return item.player;
+    });
+    //using some to check the array and using indexOf to compare, and if it exists return true
+    let isDuplicate = playersNameArray.some(function (item, index) {
+    return playersNameArray.indexOf(item) != index;
+    });
+    //if is true, we set a message for the user to see
+    if (isDuplicate) {
+    message = "Sorry, player already exist!";
+    }
+    //we filter player the duplicate using Map Filter and IndexOf
+    let results = targetTeamArray
+    .map((item) => item.player)
+    .filter(function (player, index, collection) {
+        return collection.indexOf(player) == index;
+    });
+    //once we grab the array we push it to the object
+    let resultObj = [];
+    results.forEach((item, index) => {
+    resultObj.push({ player: item });
+    });
+    //set playersArray
+    teamArray[teamIndex].playersArray = resultObj;
+    res.status(200).json({
     teamArray,
+    message,
+    });
 });
+
+app.put("/team/edit-players/:teamID", function (req, res) {
+    let teamIDNumber = Number(req.params.teamID);
+    let obj = {};
+    let teamIndex;
+    let playerIndex;
+    teamArray.forEach((team, indexTeam) => {
+    if (team.id === teamIDNumber) {
+        teamIndex = indexTeam;
+        let singleTeamArray = team.playersArray;
+        singleTeamArray.forEach((item, indexPlayer) => {
+        if (item.player === req.body.player) {
+            obj = { ...item, ...req.body };
+            playerIndex = indexPlayer;
+        }
+        });
+    }
+    });
+    teamArray[teamIndex].playersArray[playerIndex] = obj;
+    res.json(teamArray);
+});
+
+// {
+//     id: 1,
+//     name: "lakers",
+//     playersArray: [
+//     {
+//         player: "kobe",
+//         number: 24
+//     },
+//     {
+//         player: "shaq",
+//         number: 32
+//     },
+//     ],
+// },
+app.delete("/team/delete-player-by-name/:teamID", function (req, res) {
+    let teamIDNumber = Number(req.params.teamID);
+    let foundTeamPlayerArray;
+    let targetTeamIndex;
+    teamArray.forEach((item, index) => {
+    if (item.id === teamIDNumber) {
+        foundTeamPlayerArray = item.playersArray;
+        targetTeamIndex = index;
+        return;
+    }
+    });
+    // let filteredTeamPlayerArray = foundTeamPlayerArray.filter((item) => {
+    //   console.log(item);
+    //   return item.player !== req.body.player;
+    // });
+    let filteredTeamPlayerArray = foundTeamPlayerArray.filter(
+    (item) => item.player !== req.body.player
+    );
+    teamArray[targetTeamIndex].playersArray = filteredTeamPlayerArray;
+    res.send(teamArray);
 });
 
 app.listen(PORT, () => {
